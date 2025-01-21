49a92dd302c721dbab013e895e37c6bffa897d93
Shucai Xiao <shucai.xiao@amd.com>
2025-01-21 23:10:59 +0000

    add unit test for atomic_add int32

--- test_atomic_add_fp32_tensor.py
+++ test_atomic_add_fp32_tensor.py
@@ -2,6 +2,8 @@ import torch
 
 import triton  # @manual=//triton:tritonn
 import triton.language as tl  # @manual=//triton:tritonl
+import pytest
+from itertools import product
 
 
 @triton.jit
@@ -25,10 +27,10 @@ def atomic_kernel(x_ptr,
 
 def run_once(x, idx, val, block_m, block_n):
     tile_num = x.shape[0]
-    atomic_kernel[(tile_num,)](x, idx, val, x.shape[1], block_m, block_n)
+    atomic_kernel[(tile_num,)](x, idx, val, x.shape[1], block_m, block_n, num_warps=16)
 
 
-def test(row_num, column_num, block_m=32, block_n=32, dtype=torch.float32):
+def run_one_input(row_num, column_num, block_m=32, block_n=32, dtype=torch.float32):
     x = torch.zeros((row_num, column_num), dtype=dtype, device="cuda")
     idx = torch.randint(0, column_num, (block_m, block_n), dtype=torch.int32, device='cuda')
     val = torch.rand((block_m, block_n), dtype=dtype, device='cuda')
@@ -65,8 +67,29 @@ def benchmark(row_num, dtype):
 
 def main():
     benchmark.run(print_data=True, show_plots=True, save_path=".")
-    # test(1, 20, 32, 32)
+    # run_one_input(1, 20, 32, 32)
 
 if __name__ == "__main__":
     main()
 
+@pytest.mark.parametrize("row_num, atomic_size",
+                         [(r, a) for r, a in product([256 * i for i in range(1, 16)], [2, 4, 8, 16])])
+def test_correctness(row_num, atomic_size, dtype=torch.float32):
+    block_m = 32
+    block_n = 32
+    x = torch.zeros((row_num, atomic_size), dtype=dtype, device="cuda")
+    idx = torch.randint(0, atomic_size, (block_m, block_n), dtype=torch.int32, device='cuda')
+    val = torch.rand((block_m, block_n), dtype=dtype, device='cuda')
+    run_once(x, idx, val, block_m, block_n)
+
+    # calculate golden
+    golden_x = torch.zeros((row_num, atomic_size), dtype=dtype, device="cuda")
+    for j in range(block_m):
+        for k in range(block_n):
+            idx_val = idx[j][k]
+            golden_x[0][idx_val] += val[j][k]
+
+    for i in range(1, row_num, 1):
+        golden_x[i] = golden_x[0]
+
+    torch.testing.assert_close(golden_x, x)
