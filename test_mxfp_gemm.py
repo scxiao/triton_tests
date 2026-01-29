99cab20c7d75a008489a37deba9c16eb00a3f975
Shucai Xiao <shucai.xiao@amd.com>
2026-01-29 09:39:43 -0600

    refine mxfp type gemm tests

--- test_mxfp_gemm.py
+++ test_mxfp_gemm.py
@@ -13,9 +13,9 @@ def mxgemm_kernel(  #
         stride_am, stride_ak,  #
         stride_bk, stride_bn,  #
         stride_cm, stride_cn,  #
-        isfp4 :tl.constexpr,
+        fptype :tl.constexpr,
         BLOCK_M: tl.constexpr, BLOCK_N: tl.constexpr, BLOCK_K: tl.constexpr):
-    DIV_FACTOR: tl.constexpr = 2 if isfp4 else 1
+    DIV_FACTOR: tl.constexpr = 2 if fptype == 4 else 1
     pid = tl.program_id(axis=0)
     num_pid_m = tl.cdiv(M, BLOCK_M)
     pid_m = pid % num_pid_m
@@ -30,31 +30,31 @@ def mxgemm_kernel(  #
     b_ptrs = b_ptr + (offs_k[:, None] * stride_bk + offs_bn[None, :] * stride_bn)
     accumulator = tl.zeros((BLOCK_M, BLOCK_N), dtype=output_ptr.dtype.element_ty)
     k = 0
-    # for k in tl.range(0, tl.cdiv(K, BLOCK_K)):
+    for k in tl.range(0, tl.cdiv(K, BLOCK_K)):
+        k_remaining = K - k * BLOCK_K
+        valid_k = offs_k < k_remaining
+        a = tl.load(a_ptrs)#, mask=valid_k[None, :], other=0.)
+        b = tl.load(b_ptrs)#, mask=valid_k[:, None], other=0.)
+        scale_a = tl.load(a_scale_ptr)
+        scale_b = tl.load(b_scale_ptr)
+        if fptype == 4:
+            accumulator = tl.dot_scaled(a, scale_a, "e2m1", b, scale_b, "e2m1", accumulator)
+        elif fptype == 85: # e5m2
+            accumulator = tl.dot_scaled(a, scale_a, "e5m2", b, scale_b, "e5m2", accumulator)
+        else: # e4m3fn
+            accumulator = tl.dot_scaled(a, scale_a, "e4m3", b, scale_b, "e4m3", accumulator)
 
-
-    # k_remaining = K - k * BLOCK_K
-    # valid_k = offs_k < k_remaining
-    a = tl.load(a_ptrs)#, mask=valid_k[None, :], other=0.)
-    b = tl.load(b_ptrs)#, mask=valid_k[:, None], other=0.)
-    scale_a = tl.load(a_scale_ptr)
-    scale_b = tl.load(b_scale_ptr)
-    if isfp4:
-        accumulator = tl.dot_scaled(a, scale_a, "e2m1", b, scale_b, "e2m1", accumulator)
-    else:
-        accumulator = tl.dot_scaled(a, scale_a, "e5m2", b, scale_b, "e5m2", accumulator)
-
-    # a_ptrs += BLOCK_K * stride_ak
-    # b_ptrs += BLOCK_K * stride_bk
-    # a_scale_ptr += BLOCK_K // 32
-    # b_scale_ptr += BLOCK_K // 32
+        a_ptrs += (BLOCK_K // DIV_FACTOR) * stride_ak
+        b_ptrs += (BLOCK_K // DIV_FACTOR) * stride_bk
+        a_scale_ptr += BLOCK_K // 32
+        b_scale_ptr += BLOCK_K // 32
 
 
     offs_cm = pid_m * BLOCK_M + tl.arange(0, BLOCK_M)
     offs_cn = pid_n * BLOCK_N + tl.arange(0, BLOCK_N)
     output_ptrs = output_ptr + stride_cm * offs_cm[:, None] + stride_cn * offs_cn[None, :]
     c_mask = (offs_cm[:, None] < M) & (offs_cn[None, :] < N)
-    tl.store(output_ptrs, accumulator)#, mask=c_mask)
+    tl.store(output_ptrs, accumulator, mask=c_mask)
 
 
 def fp8e8m0_to_float32(scale):
@@ -68,17 +68,11 @@ def fp8e8m0_to_float32(scale):
 def torch_gemm_mxfp(a, b, a_scale, b_scale, dtype_src_str):
     a_scale_f32 = fp8e8m0_to_float32(a_scale)
     b_scale_f32 = fp8e8m0_to_float32(b_scale)
-    print(f"a_scale ={a_scale_f32}")
-    print(f"b_scale = {b_scale_f32}")
 
     a_scale_f32 = a_scale_f32.repeat_interleave(32, dim=1)
     b_scale_f32 = b_scale_f32.repeat_interleave(32, dim=1)
     a_f32 = a.to(torch.float32)
     b_f32 = b.to(torch.float32)
-
-    print(f"a_f32 = {a_f32}")
-    print(f"b_f32 = {b_f32}")
-
 
     # b_scales are always col major
     b_scale_f32 = b_scale_f32.T.contiguous()
@@ -86,10 +80,8 @@ def torch_gemm_mxfp(a, b, a_scale, b_scale, dtype_src_str):
     a = a_f32 * a_scale_f32
     b = b_f32 * b_scale_f32
 
-    print(f"a = {a}")
-    print(f"b = {b}")
-
     ref_out = torch.matmul(a, b).to(torch.float32)
+
     return ref_out
 
 
@@ -110,10 +102,12 @@ def get_ptype(dtype):
 
 def generate_configs():
     base_configs = [
-        {"M": 32, "N": 32, "K": 128, "BLOCK_M": 32, "BLOCK_N":32, "BLOCK_K":128, "NUM_WARPS": 1, "NUM_CTAS": 1},
+        {"M": 1024, "N": 1024, "K": 1024, "BLOCK_M": 64, "BLOCK_N":64, "BLOCK_K":128, "NUM_WARPS": 4, "NUM_CTAS": 1, "DTYPE": 'float4'},
+        {"M": 1024, "N": 1024, "K": 1024, "BLOCK_M": 64, "BLOCK_N":64, "BLOCK_K":128, "NUM_WARPS": 4, "NUM_CTAS": 1, "DTYPE": 'float8_e4m3fn'},
+        {"M": 1024, "N": 1024, "K": 1024, "BLOCK_M": 64, "BLOCK_N":64, "BLOCK_K":128, "NUM_WARPS": 4, "NUM_CTAS": 1, "DTYPE": 'float8_e5m2'},
     ]
-
     return base_configs
+
 
 def triton_gemm_mxfp(config):
     M = config['M']
@@ -122,24 +116,24 @@ def triton_gemm_mxfp(config):
     BLOCK_M = config['BLOCK_M']
     BLOCK_N = config['BLOCK_N']
     BLOCK_K = config['BLOCK_K']
-    NUM_CTAS = config['NUM_CTAS']
-    cStrideM = 1
+    # NUM_CTAS = config['NUM_CTAS']
+    num_warps = config['NUM_WARPS']
+    dtype = config['DTYPE']
+    num_stages = 2
 
-    dtype = "float8_e5m2"
-    # dtype = "float4"
-    kernel_file = "mxgemm_kernel"
-    outdir = "mxgemm_kernel"
-    num_stages = 1
 
-    isfp4 = 1 if (dtype=="float4") else 0
-    ptype = get_ptype(dtype=dtype)
+    fp_type_flag = 4 
+    if dtype=="float8_e4m3fn":
+        fp_type_flag = 84
+    elif dtype == "float8_e5m2":
+        fp_type_flag = 85
 
     torch.manual_seed(42)
     torch.set_printoptions(edgeitems=30, linewidth=100000)
     if (dtype != "float4"):
         torch_type = getattr(torch, dtype)
-        a = (torch.randint(1, 6, (M, K))).to(torch_type)
-        b = (torch.randint(1, 6, (K, N))).to(torch_type)
+        a = (torch.randint(1, 10, (M, K))).to(torch_type)
+        b = (torch.randint(1, 10, (K, N))).to(torch_type)
     else:
         a = MXFP4Tensor(data = torch.randint(1,5, (M,K)))
         b = MXFP4Tensor(data = torch.randint(1,5, (K,N)))
@@ -169,12 +163,12 @@ def triton_gemm_mxfp(config):
         a.stride(0), a.stride(1),  #
         b.stride(0), b.stride(1),  #
         c.stride(0), c.stride(1),  #
-        isfp4,
+        fp_type_flag,
         BLOCK_M, BLOCK_N, BLOCK_K,  #
         num_stages=num_stages,
-        num_warps=2)
+        num_warps=num_warps)
 
     torch.testing.assert_close(c.cpu(), c_ref)
 
 if __name__ == '__main__':
-    triton_gemm_mxfp(generate_configs()[0])
+    triton_gemm_mxfp(generate_configs()[2])
